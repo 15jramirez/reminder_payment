4820d1dbb65cdddfd438b51b8d2f2c9c9be3906b
jramirez <ramirez.jocelyn73@yahoo.com>
2020-12-10 15:40:29 -0800

    made user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,11 +20,12 @@ class ApplicationController < Sinatra::Base
 
    #helper methods
    helpers do 
-    def logged_in
-        !!session[:user_id]
-    end
+        def logged_in
+            !!session[:user_id]
+        end
 
-    def current_user
-        User.find(session[:user_id])
+        def current_user
+            User.find(session[:user_id])
+        end
     end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User <ActiveRecord::Base
+    has secure_password
+end
