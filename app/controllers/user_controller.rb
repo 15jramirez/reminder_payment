5b7d29e77192ead9cdba7c75604b8876f56d0787
jramirez <ramirez.jocelyn73@yahoo.com>
2020-12-10 16:44:06 -0800

    built form for sign up

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,44 @@
+class UserController < ApplicationController
+    #create method
+    get '/signup' do 
+        erb :'users/signup'
+    end
+
+    post '/account' do 
+        #create an instance of user
+            #going to save view create method
+        #add session to user
+        #redirect this to account? 
+    end
+
+    #login 
+    get '/login' do
+    erb :'users/login'
+    end
+    
+    post '/account' do
+        #find by session of user
+        #raise an error if incorrect
+        #redirect to account
+    end
+
+    #bonus (if have time)
+
+    get '/account/settings' do
+        #here will show users their email, password, and username
+        #at bottom given option to edit account
+        # erb :'users/settings'
+    end
+
+    get '/account/settings/edit' do 
+        #this will allow to edit the page
+        # erb :'users/settings'
+    end
+
+    # PATCH '/account/settings/' do 
+    #     look up on how to edit a user account
+    #     redirect to '/account/settings'
+        
+    # end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
     has_many :payments
-    has secure_password
+    # has secure_password
 end
