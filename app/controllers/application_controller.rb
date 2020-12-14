c7aebe661ee538d9c5c8396e3c731edeb73b1861
jramirez <ramirez.jocelyn73@yahoo.com>
2020-12-13 20:43:51 -0800

    removed public folder from configure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 require './config/environment'
 class ApplicationController < Sinatra::Base
-   configure do 
-    set :public_folder, 'public'
+   configure do
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "password_security"
